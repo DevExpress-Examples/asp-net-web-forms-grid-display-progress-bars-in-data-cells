fb561ccd60db4bf9fecf8a58b6105b3ed77269a2
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,24 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Web.ASPxGridView
+Imports DevExpress.Web.ASPxEditors
+Imports System.Drawing
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub pb_DataBound(ByVal sender As Object, ByVal e As EventArgs)
+		Dim progressBar As ASPxProgressBar = CType(sender, ASPxProgressBar)
+
+		If progressBar.Position > 100 Then
+			progressBar.Position = 100
+		End If
+
+		If progressBar.Position < 40 Then
+			progressBar.IndicatorStyle.BackColor = Color.LightGreen
+		ElseIf progressBar.Position >= 40 AndAlso progressBar.Position < 75 Then
+			progressBar.IndicatorStyle.BackColor = Color.Yellow
+		ElseIf progressBar.Position >= 75 Then
+			progressBar.IndicatorStyle.BackColor = Color.Red
+		End If
+	End Sub
+End Class
