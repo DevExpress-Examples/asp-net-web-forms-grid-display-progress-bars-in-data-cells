a0d946a4012991ba7a7c3b8b28c3a24be61fbe6b
DevExpressExampleBot <fakeEmail>
2022-12-07 14:41:52 +0400

    Source auto update [skip ci]

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using DevExpress.Web.ASPxGridView;
-using DevExpress.Web.ASPxEditors;
+using DevExpress.Web;
 using System.Drawing;
 
 public partial class _Default : System.Web.UI.Page {
